6cf3a676e2607b122d05c840d4f89cd49e246bf0
Kyo <kyosenju1522@gmail.com>
2023-11-10 20:13:19 -0300

    ..

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -6,8 +6,8 @@ interface FormInputProps {
 
 const FormInput: React.FC<FormInputProps> = (props) => {
   return (
-    <form className="flex flex-col gap-y-2">
-      <label htmlFor="name" className="text-white">
+    <div className="flex flex-col gap-y-2">
+      <label htmlFor={props.inputId} className="text-white">
         <span>{props.inputName}</span>
       </label>
       <input
@@ -16,7 +16,7 @@ const FormInput: React.FC<FormInputProps> = (props) => {
         id={props.inputId}
         className="input bg-neutral-800 text-white w-80"
       />
-    </form>
+    </div>
   );
 };
 
