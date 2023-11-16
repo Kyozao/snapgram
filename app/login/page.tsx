c4f1c9a583d5d8a357427061124788b9092d99f4
Kyo <kyosenju1522@gmail.com>
2023-11-16 00:09:56 -0300

    ...

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -41,6 +41,7 @@ export default function Home() {
               dark: {
                 colors: {
                   defaultButtonBorder: "transparent",
+                  
                 },
               },
             },
