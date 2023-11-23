73444f7c0c18b42b2d5ed6e0351c92df02897a5e
Kyo <kyosenju1522@gmail.com>
2023-11-22 21:12:12 -0300

    ...

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -24,25 +24,7 @@ export default function Home() {
         <section className="text-center">
           <h1 className="text-white font-bold text-2xl">Log in your account</h1>
           <p className="text-violet-200 text-sm">
-            To use Snapgram please enter your details Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Dolor voluptas, ut eveniet at,
-            architecto nostrum vero ipsam illo atque fuga pariatur voluptate
-            numquam. Fugiat ipsam tempore iure facere repellat nulla? Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-            reprehenderit excepturi expedita voluptatum sint, molestiae rerum, a
-            quos sunt voluptatem odio sed minima delectus ab nobis aspernatur
-            nostrum saepe doloribus? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Sunt cum quidem architecto libero eius nemo ad
-            voluptatibus incidunt molestiae, officia labore voluptatem
-            consequatur corporis ex provident molestias sequi quae maxime.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, cum
-            commodi iste nihil, autem repudiandae voluptates harum rem
-            praesentium incidunt necessitatibus suscipit doloremque, debitis
-            neque nisi. Labore sed repellendus quia? Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Beatae consectetur veritatis, dolorem
-            aliquid molestias quos! Nisi, nobis, veritatis, facere debitis
-            reiciendis soluta id voluptas officiis dolorum fugiat atque quisquam
-            ex.
+            To use Snapgram please enter your details
           </p>
         </section>
         <Auth
